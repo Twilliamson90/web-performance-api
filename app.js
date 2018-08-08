ed2fb10286a6c0c3853a92a606b14751421b6c82
Travis Williamson <Twilliamson90@gmail.com>
2018-08-07 20:56:45 -0400

    Setup database connection

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
-// const lighthouse = require('./lighthouse.js');
-const app = express();
-
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-db.json");
+
+const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://web-pref-leaderboard.firebaseio.com"
+});
+
+const db = admin.database();
+let ref = db.ref("/boards");
+
+ref.on('value', gotData, errData);
+
+function gotData(data) {
+  console.log('Got data!');
+  console.log(data.val());
+}
+
+function errData(err) {
+  console.log('Error!');
+  console.log(err);
+}
+
+console.log(ref);
+
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
@@ -23,15 +47,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/boards", (req, res) => {
-  res.send('Jello');
 
   const opts = {
-    chromeFlags: ['--show-paint-rects']
+    chromeFlags: ['--headless'],
   };
 
+  const testURL = 'https://travisw.me';
+
   // Usage:
-  launchChromeAndRunLighthouse('https://example.com', opts).then(results => {
-    console.log(results);
+  launchChromeAndRunLighthouse(testURL, opts).then(results => {
+    console.log(testURL);
+    console.log(results.audits['speed-index'].rawValue);
+    res.json(results.audits['speed-index'].rawValue);
   });
 
 });
